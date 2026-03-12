21609fb2612fef978afbde0b0699fb22fc9f2f7d
Pedro Augusto <pedrosousaugustooo@gmail.com>
2026-03-12 16:14:52 +0000

    Vou dar continuidade ao codigo mais tarde.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,27 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char Estado, //Uma letra de 'A' a 'H' (representando um dos oito estados).
+      CodigodaCarta, //A letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
+      NomedaCidade[30]; // O nome da cidade
+
+  int População, // O número de habitantes da cidade.
+    NumerodePontosTuristicos; // A quantidade de pontos turísticos na cidade.
+
+  float Areaemkm, // A área da cidade em quilômetros quadrados.
+    PIB; //O Produto Interno Bruto da cidade
 
   // Área para entrada de dados
+
+  printf ("Insira o Estado: \n");
+  scanf ( "%c", &Estado);
+  printf ("Insira o Código: \n");
+  scanf ( "%c", &CodigodaCarta);
+  printf ("Insira o Nome da Cidade: \n");
+  scanf ("%c", NomedaCidade);
+
+
+
 
   // Área para exibição dos dados da cidade
 
