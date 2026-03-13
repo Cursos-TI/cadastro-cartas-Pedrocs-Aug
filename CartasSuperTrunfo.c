112878dc7696b804c66d244cc76a443a73b3b094
Pedro Augusto <pedrosousaugustooo@gmail.com>
2026-03-13 10:44:34 +0000

    O jogo está pronto: Adiciona configuração de tarefas para compilação em C e implementa cadastro de cartas no jogo Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,31 +4,45 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
+int main()
+{
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char Estado, //Uma letra de 'A' a 'H' (representando um dos oito estados).
-      CodigodaCarta, //A letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
+  char Estado,          // Uma letra de 'A' a 'H' (representando um dos oito estados).
+      CodigodaCarta[4],    // A letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
       NomedaCidade[30]; // O nome da cidade
 
-  int População, // O número de habitantes da cidade.
-    NumerodePontosTuristicos; // A quantidade de pontos turísticos na cidade.
+  int População,                // O número de habitantes da cidade.
+      NumerodePontosTuristicos; // A quantidade de pontos turísticos na cidade.
 
   float Areaemkm, // A área da cidade em quilômetros quadrados.
-    PIB; //O Produto Interno Bruto da cidade
+      PIB;        // O Produto Interno Bruto da cidade
 
   // Área para entrada de dados
 
-  printf ("Insira o Estado: \n");
-  scanf ( "%c", &Estado);
-  printf ("Insira o Código: \n");
-  scanf ( "%c", &CodigodaCarta);
-  printf ("Insira o Nome da Cidade: \n");
-  scanf ("%c", NomedaCidade);
-
-
-
+  printf("Insira o Estado: \n");
+  scanf("%c", &Estado);
+  printf("Insira o Código: \n");
+  scanf("%s", CodigodaCarta);
+  printf("Insira o Nome da Cidade: \n");
+  scanf("%s", NomedaCidade);
+  printf("Insira a quantidade populacional \n");
+  scanf("%i", &População);
+  printf("Insira a área da cidade em quilômetros quadrados \n");
+  scanf("%f", &Areaemkm);
+  printf("Insira o Produto Interno Bruto da cidade  \n");
+  scanf("%f", &PIB);
+  printf("Insira a quantidade de pontos turisticos \n");
+  scanf("%i", &NumerodePontosTuristicos);
 
   // Área para exibição dos dados da cidade
 
-return 0;
-} 
+  printf("Estado: %c \n", Estado);
+  printf("Código: %sa \n", CodigodaCarta);
+  printf("Nome da Cidade: %s \n", NomedaCidade);
+  printf("População: %i \n", População);
+  printf("Área: %2.f km² \n", Areaemkm);
+  printf("PIB: %2.f bilhões de reais \n", PIB);
+  printf("Número de Pontos Turísticos: %i \n", NumerodePontosTuristicos);
+
+  return 0;
+}
